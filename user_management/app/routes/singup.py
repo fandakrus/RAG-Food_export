6776547e480d1f0fd642fe23e0d8b3a0a09a7af9
František Krůs <krusfran@fit.cvut.cz>
2024-07-11 13:06:12 +0700

    Basic checkes added

--- user_management/app/routes/singup.py
+++ user_management/app/routes/singup.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, request, jsonify
 from .. import db, bcrypt
-from ..models import User
+from ..models import User, Allowed_user
 
 auth_bp = Blueprint('auth', __name__)
 
@@ -13,11 +13,23 @@ def signup():
 
 	if not email or not name or not password:
 		return jsonify({'success': False, 'message': 'All fields are required'}), 400
+	
+	existing_user = User.query.filter_by(email=email).first()
+	if existing_user:
+		return jsonify({'success': False, 'message': 'User with this email address already exists'}), 400
+	
+	allowed_user = Allowed_user.query.filter_by(email=email).first()
+	if not allowed_user:
+		return jsonify({'success': False, 'message': 'This email is not allowed to sign up'}), 400
 
 	hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
 
 	new_user = User(email=email, name=name, password=hashed_password)
-	db.session.add(new_user)
-	db.session.commit()
+	try:
+		db.session.add(new_user)
+		db.session.commit()
+	except Exception as e:
+		print(f"Error adding user to the database: {e}")
+		return jsonify({'success': False, 'message': 'Unexpected error occurred'}), 500
 
 	return jsonify({'success': True, 'message': 'User registered successfully'}), 201
