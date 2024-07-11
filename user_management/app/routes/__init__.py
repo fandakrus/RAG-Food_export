9921fab9876c11e02737b594a2b772dd887d1671
František Krůs <krusfran@fit.cvut.cz>
2024-07-11 11:51:54 +0700

    App is now rendering templates correctly without js

--- user_management/app/routes/__init__.py
+++ user_management/app/routes/__init__.py
@@ -3,6 +3,8 @@ from flask import Blueprint
 def init_routes(app):
     from .singup import auth_bp
     from .singup_page import signup_page_bp
+    from .email_verify_page import email_verify_page_bp
 
     app.register_blueprint(auth_bp)
     app.register_blueprint(signup_page_bp)
+    app.register_blueprint(email_verify_page_bp)
