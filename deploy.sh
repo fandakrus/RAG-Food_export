940f56ffbfb6354bfc5d7a6b6c73145ea05ccaae
František Krůs <krusfran@fit.cvut.cz>
2024-08-05 14:00:14 +0700

    Demployment changes

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+git clone https://github.com/fandakrus/RAG-Food_export.git
+sudo apt update
+sudo apt install python3 python3-pip
+sudo apt install python3-venv
+cd RAG-Food_export/
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
