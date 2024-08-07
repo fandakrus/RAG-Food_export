a483b587eff1e6601b3e5d923add0b4868c1d5e7
František Krůs <krusfran@fit.cvut.cz>
2024-08-07 10:00:33 +0700

    Deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -8,3 +8,14 @@ source venv/bin/activate
 pip install -r requirements.txt
 pip install gunicorn
 sudo apt install nginx
+mv ~/deploy_config/chatbot.service /etc/systemd/system/chatbot.service
+mv ~/deploy_config/user_manag.service /etc/systemd/system/user_manag.service
+sudo systemctl start chatbot
+sudo systemctl enable chatbot
+sudo systemctl start user_manag
+sudo systemctl enable user_manag
+mv ~/deploy_config/nginx_config /etc/nginx/sites-available/myproject
+sudo ln -s /etc/nginx/sites-available/myproject /etc/nginx/sites-enabled
+sudo nginx -t
+sudo systemctl restart nginx
+sudo ufw allow 'Nginx Full'
